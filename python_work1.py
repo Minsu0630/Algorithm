63d98e46ef8a98849862c0f41658c88f31bdde81
Minsu0630 <kwonminsu0630@naver.com>
2025-11-10 00:47:59 +0900

    Update python_work1.py

--- python_work1.py
+++ python_work1.py
@@ -1,17 +1,17 @@
-A = input().split()
-N = int(A[0])
-M = int(A[1])
+a = input().split()
+n = int(A[0])
+m = int(A[1])
 
 basket = []
-for i in range(N):
+for i in range(n):
     basket.append(i+1)
 
-for i in range(M):
-    Mlist = input().split()
-    num1 = int(Mlist[0])
-    num2 = int(Mlist[1])
+for i in range(m):
+    m_list = input().split()
+    num1 = int(m_list[0])
+    num2 = int(m_list[1])
 
     basket[num1-1], basket[num2-1] = basket[num2-1], basket[num1-1]
 
-for i in range(N):
+for i in range(n):
     print(basket[i], end=" ")
