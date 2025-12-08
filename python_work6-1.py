5fe4cf1a7aeeaa63ecb42f51702f844ee0c1f086
Minsu0630 <kwonminsu0630@naver.com>
2025-12-08 09:15:05 +0900

    Update python_work6-1.py

--- python_work6-1.py
+++ python_work6-1.py
@@ -1,16 +1,7 @@
-import sys
-
-def solve():
-    board = sys.stdin.readline().strip()
-    
-    # 간편한 치환법: AAAA를 먼저 채우고, 남은 X를 BB로 채움
-    board = board.replace("XXXX", "AAAA")
-    board = board.replace("XX", "BB")
-    
-    # 여전히 X가 남아있다면 덮을 수 없는 부분이 있다는 뜻
-    if 'X' in board:
-        print("-1")
-    else:
-        print(board)
-
-solve()
+board = input()
+board = board.replace("XXXX", "AAAA")
+board = board.replace("XX", "BB")
+if 'X' in board:
+    print("-1")
+else:
+    print
